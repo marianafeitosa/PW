4f2df6df8d4d36bf7bc7ce2f2ebe3ac7d5305836
mar¡ <99839074+marianafeitosa@users.noreply.github.com>
2023-08-21 16:33:32 -0300

    Add files via upload

---
+++ SISTEMAWEB-21-08/conexao.php
@@ -0,0 +1,23 @@
+<?php
+define('SERVIDOR', 'localhost');
+define('USUARIO', 'root');
+define('SENHA', '');
+define('BANCO', 'projetoweb');
+
+class Conexao{
+    protected $mysqli;
+    public function __construct(){
+        $this->conexaoMysql();
+  
+    }
+    public function conexaoMysql(){
+        $this->mysqli = new mysqli(SERVIDOR, USUARIO, SENHA, BANCO);
+    }
+    public function setAgedamentos($nome, $telefone, $origem, $data_contato, $observacao){
+        $stmt = $this->mysqli->prepare("INSERT INTO agendamentos('nome','telefone','origem','data_contato','observacao') VALUES(?,?,?,?,?)");
+        $stmt->bind_param("sssss",$nome,$telefone,$origem,$data_contato,$observacao);
+        $stmt->execute();
+    }
+}
+
+?>
---
+++ SISTEMAWEB-21-08/resposta.php
@@ -0,0 +1,19 @@
+<?php
+
+ 
+
+$nome = $_POST['txtNome'];
+
+$Telefone = $_POST['txtTelefone'];
+
+$origem = $_POST['txtOrigem'];
+
+$Data = $_POST['txtData_contato'];
+
+$observacao = $_POST['txtObservacao'];
+
+ 
+
+echo $nome
+
+?>
